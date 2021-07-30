75a2d71f5ba7285cc7ecaf3221e8553b7a4c560d
guny <pc-van@nifty.com>
2021-07-30 15:51:50 +0900

    「次のステータスへ移動」ボタンの動作完了

--- shift_status.js
+++ shift_status.js
@@ -2,7 +2,10 @@
 
 $(function() {
     var ActiveTrackers = ["1.DR情報‗駆動S/W開発G"]; //「次ステータスへ移動」ボタン追加トラッカー
+    var FinishStatus = 14; // 子チケットの完了ステータス番号
 
+    // 編集画面からステータスを削除
+    $(".splitcontentleft p:first").hide();
 
     // 該当トラッカーのとき
     if ($.inArray($("#issue_tracker_id option:selected").text(), ActiveTrackers) != -1) {
@@ -31,7 +34,6 @@ $(function() {
         // 「次ステータスへ移動」ボタン
         var status_next_button = '<button id="status-next-button">次ステータスへ移動</button>';
 
-
         switch (Role) {
             case "Creater":
                 if (CurrentStatusStr == "記入") {
@@ -52,10 +54,20 @@ $(function() {
         // 「次ステータスへ移動」ボタン押下
         $("#status-next-button").on("click", function() {
             function SetNextStatus() {
-                var next_status_id = $("#issue_status_id option:last").val();
+                var StatusIdForWorkFrow = [13, 11, 14]; //記入->完了回覧中->完了
+                var NextStatusId;
+
+                switch (CurrentStatusStr) {
+                    case "記入":
+                        NextStatusId = 11; // 次は「完了回覧中」
+                        break;
+                    case "完了回覧中":
+                        NextStatusId = 14; // 次は「完了」
+                        break;
+                }
                 var send_data = {
                     "issue": {
-                        "status_id": next_status_id
+                        "status_id": NextStatusId
                     }
                 };
 
@@ -71,7 +83,34 @@ $(function() {
                         function(data, textStatus, jqXHR) {
                             location.reload();
                         },
-                        function(jqXHR, textStatus, errorThrown) {});
+                        function(jqXHR, textStatus, errorThrown) {
+                            location.reload();
+                        });
+            }
+
+            function SetNextStatus_Child(TicketNo) {
+                var send_data = {
+                    "issue": {
+                        "status_id": FinishStatus
+                    }
+                };
+
+                $.ajax({
+                        headers: { 'X-Redmine-API-Key': ViewCustomize["context"]["user"]["apiKey"] },
+                        type: "PUT",
+                        url: "/redmine/issues/" + TicketNo + ".json",
+                        dataType: "json",
+                        contentType: 'application/json',
+                        data: JSON.stringify(send_data)
+                    })
+                    .then(
+                        function(data, textStatus, jqXHR) {
+                            SetNextStatus();
+                            location.reload();
+                        },
+                        function(jqXHR, textStatus, errorThrown) {
+                            location.reload();
+                        });
             }
 
             switch (Role) {
@@ -105,12 +144,15 @@ $(function() {
                                             })
                                             .then(
                                                 function(data, textStatus, jqXHR) {
+                                                    // カスタムフィールドを調査
                                                     for (cnt = 0; cnt < data["issue"]["custom_fields"].length; cnt++) {
                                                         fld = data["issue"]["custom_fields"][cnt];
                                                         if (fld["name"] == "指摘者") {
+                                                            // 自分が指摘者の時
                                                             if (fld["value"] == CurrentUserId) {
                                                                 // 子チケットを完了にする
-                                                                // SetNextStatus関数でチケット番号を指定できるようにする
+                                                                SetNextStatus_Child(data["issue"]["id"]);
+                                                                break;
                                                             }
                                                         }
                                                     }
